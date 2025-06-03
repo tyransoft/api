4c0f4f8dd6a8e6f77c93a31c3bb3768ac68c6869
tyransoft <tyransoftagency@gmail.com>
2025-06-03 10:24:34 +0200

    pro3_dtits3db

--- project/project/settings.py
+++ project/project/settings.py
@@ -65,15 +65,24 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'shahenat_shahenatiapidb', 
+        'USER': 'shahenat_loremdb',  
+        'PASSWORD': 'OXhB!P;he8',  
+        'HOST': '102.213.180.2',  
+        'PORT': ''
     }
-}
-
-
-
+} 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
